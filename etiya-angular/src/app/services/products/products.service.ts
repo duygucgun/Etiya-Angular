d96e56e7e515606ec8508b2251086c37b4b0f2a4
Duygu Uçgun <duygucgun@gmail.com>
2022-07-29 15:06:56 +0300

    etiya-angular

---
+++ etiya-angular/src/app/services/products/products.service.ts
@@ -0,0 +1,25 @@
+import { getLocaleFirstDayOfWeek } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import Product from 'src/app/models/product';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  apiControllerUrl:string = `${environment.apiUrl}/products`;
+
+  constructor(private httpClient:HttpClient) { } //http client'ı injecte etmeliyiz
+
+  getList(): Observable<Product[]> {
+    // subscribe yapmalıyım çünkü gelen cevabı beklemeliyim asenkron çalıştığı için
+    return this.httpClient.get<Product[]>(`${this.apiControllerUrl}`);
+  }
+
+  getById(id:number): Observable<Product> {
+    return this.httpClient.get<Product>(`${this.apiControllerUrl}/${id}}`)
+  }
+}
+
